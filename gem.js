be73b1014194ba0671b9858daa7eec2f7ec019db
B Barnett <justbbarnett@Bs-MacBook-Air.local>
2018-02-22 15:38:47 -0600

    working gem game, but still working on star wars RPG

---
+++ gem.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+
+    //The Incredibles family
+
+    var characters = [ "Mr", "Mrs", "Violet", "Dash", "Edna"];
+    var powerRate = [10, 8, 6, 5, 13];
+    var counter = 0;
+    var targetNumber = 103;
+    //  random  = Math.floor(Math.random() * 150 + 75)
+
+
+    var wins = 0;
+    var losses = 0;
+
+    console.log(characters)
+    console.log(characters.length)
+    console.log(powerRate)
+    
+    for (var i =0; i<characters.length; i++) {
+    
+        var charImage = $("<img>");
+        charImage.addClass("character-image");
+        charImage.attr("src", "images/"+characters[i]+".png");
+
+        charImage.attr("data-power", powerRate[i]);
+        $("#characters-section").append(charImage);
+       }
+
+    $(".character-image").on("click", function(){
+
+        var power = ($(this).attr("data-power"));
+        power = parseInt(power);
+        counter += power
+
+        
+        var newScore = "<p>Current number: " + counter + "</p>"
+            document.querySelector("#counter").innerHTML = newScore;
+        
+
+        var targetHTML = "<p>Get to number: " + targetNumber + "</p>"
+            document.querySelector("#target-number").innerHTML = targetHTML;
+
+
+        if (counter === targetNumber) {
+          wins++, counter = 0;
+        }
+        else if (counter >= targetNumber) {
+          losses++, counter = 0;
+        }
+        var record = "<p>Wins: " + wins + "</p>" +
+                    "<p>Losses: " + losses + "</p>";
+
+            document.querySelector("#history").innerHTML = record;
+
+    })
+
+});
---
+++ wannabeRPG/wannabe.js
@@ -0,0 +1,98 @@
+
+       var characters = {
+        "mr" : {
+            name: "Mr Incredible",
+            health: 100,
+            offense: 20,
+            defense: 20,
+            image: "../images/mr.png"
+        },
+        "mrs" : {
+            name: "Ms incredible",
+            health: 120,
+            offense: 15,
+            defense: 10,
+            image: "../images/mrs.png"
+        },
+        "violet": {
+            name: "Violet",
+            health: 150,
+            offense: 10,
+            defense: 5,
+            image: "../images/violet.png"
+        },
+        "dash" : {
+            name: "Dash",
+            health: 170,
+            offense: 12,
+            defense: 7,
+            image: "../images/dash.png"
+        },
+        "edna" : {
+            name: "Edna",
+            health: 80,
+            offense: 25,
+            defense: 20,
+            image: "../images/edna.png"
+        },
+    };
+
+    console.log(characters)
+    console.log(characters.length)
+    console.log(characters.edna.health)
+
+    
+    //things the game needs to count
+    var currCharacter;
+    var currDefender;
+    var defenders = [];
+    var battleResults;
+    var attackCount = 0;
+    
+    
+    
+    
+    // var render = function (character, renderArea) {
+    //     //character = object, renderArea = class, createChar = string
+
+    //     //define each "var" in the character div - character now defined as the object to hold
+    //     var charDiv = $( "<div class='character' data-name='"+ character.name + "'>");
+    //     var charName = $( "<div class='character-name'>").text(character.name);
+    //     var charImage = $( "<img alt='Icredible character of your choice' class='character-image'>").attr("src", character.image);
+    //     var charHealth = $( "<div class='character-text'>").attr(character.health);
+
+    //     charDiv.append(charName).append(charImage).append(charHealth); 
+    //     //this should display everything you just coded
+
+    //     // if (charStatus == "defender") {
+    //     //     $(charDiv).addClass("defender");
+    //     // }
+    //     // else if (charStatus == "target") {
+    //     //     currDefender = character;
+    //     //     $(charDiv).addClass("target");
+    //     // }
+
+    // };
+
+    for( var key in characters ){
+        let thisCharacter = characters[key].name;
+        console.log(thisCharacter);
+    }
+
+    for (var x in characters) {
+        // var charDiv = $( "<div>");
+        // charDiv.addClass ("character");
+        // charDiv.attr ("data-name", characters[x].name);
+
+
+        var charDiv = $("<div class='card'>")
+        var charName = $( "<div class='character-name'>").text(characters[x].name);
+        var charImage = $( "<img alt='Icredible character of your choice' class='character-image'>").attr("src", characters[x].image);
+        var charHealth = $( "<div class='character-text'>").text(characters[x].health);
+        
+        charDiv.append(charName).append(charImage).append(charHealth);
+
+        $(".character-section").append(charDiv).addClass ("character");
+
+        
+    }
